774cdcd35d3b7950b68e4f3861ce180bc602c6a4
thakur295 <saurabh.thakur295@gmail.com>
2017-01-26 23:15:46 +0530

    Update media.py

--- media.py
+++ media.py
@@ -9,10 +9,10 @@ value = ast.literal_eval(output)
 pk = movie.Movie(value.get('Title'),value.get('Plot'),value.get('Poster'),"https://www.youtube.com/watch?v=SOXWc32k4zA",value.get('Year'),value.get('imdbRating'))
 connection.close()
 
-connection = urllib.urlopen("http://www.omdbapi.com/?t=airlift&y=2016&plot=short&r=json")
+connection = urllib.urlopen("http://www.omdbapi.com/?i=tt0871510&plot=short&r=json")
 output = connection.read()
 value = ast.literal_eval(output)
-AirLift = movie.Movie(value.get('Title'),value.get('Plot'),value.get('Poster'),"https://www.youtube.com/watch?v=vb5xCMbMfZ0",value.get('Year'),value.get('imdbRating'))
+chakDeIndia = movie.Movie(value.get('Title'),value.get('Plot'),value.get('Poster'),"https://www.youtube.com/watch?v=6a0-dSMWm5g",value.get('Year'),value.get('imdbRating'))
 connection.close()
 
 connection = urllib.urlopen("http://www.omdbapi.com/?t=bajrangi&y=2015&plot=short&r=json")
@@ -39,6 +39,6 @@ value = ast.literal_eval(output)
 madari = movie.Movie(value.get('Title'),value.get('Plot'),value.get('Poster'),"https://www.youtube.com/watch?v=j4s3JmLGLCA",value.get('Year'),value.get('imdbRating'))
 connection.close()                 
 
-movies = [pk,AirLift,BajrangiBhaijaan,Baahubali,msd,madari]
+movies = [pk,chakDeIndia,BajrangiBhaijaan,Baahubali,msd,madari]
 fresh_tomatoes.open_movies_page(movies)
 
