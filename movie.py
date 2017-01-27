281ac8b4dadb130d86e4a2f0b45282096c8e6dc4
thakur295 <saurabh.thakur295@gmail.com>
2017-01-27 15:17:44 +0530

    Delete movie.py

--- movie.py
+++
@@ -1,9 +0,0 @@
-import webbrowser
-class Movie():
-    def __init__(self,title,story_line,poster,trailor,year,rating):
-        self.title = title
-        self.storyline = story_line
-        self.poster_image_url = poster
-        self.trailer_youtube_url = trailor
-        self.year = year
-        self.rating = rating
